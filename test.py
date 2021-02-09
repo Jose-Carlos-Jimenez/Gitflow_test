5b406f8595a45de6eb5fc7c2187b7f6fd7749f09
Jose Carlos Jimenez <55406693+Jose-Carlos-Jimenez@users.noreply.github.com>
2021-02-09 14:00:29 -0600

    Adding visual ackerman function

--- test.py
+++ test.py
@@ -9,3 +9,14 @@ def merryChristmas(n):
         print (' '*(n - (x + 1)), '*'*(2*x +1))
 
 merryChristmas(10)
+
+def ackerman(m, n, s="%s"):
+    print (s % ("A(%d,%d)" % (m, n)))
+    if m == 0:
+        return n + 1
+    if n == 0:
+        return ackerman(m - 1, 1, s)
+    n2 = ackerman(m, n - 1, s % ("A(%d,%%s)" % (m - 1)))
+    return ackerman(m - 1, n2, s)
+
+print(ackerman(3,2))
