1500005e041e2fc15d5313b11c12caa85c4a9411
Jose Carlos Jimenez <55406693+Jose-Carlos-Jimenez@users.noreply.github.com>
2021-02-09 13:51:09 -0600

    Commit on hotfix 1.1

--- test.py
+++ test.py
@@ -2,3 +2,10 @@ print("Hola mundo desde la rama develop.")
 
 for x in range(10):
     print("Hola ", x, " desde el release.")
+
+
+def merryChristmas(n):
+    for x in range(n):
+        print (' '*(n - (x + 1)), '*'*(2*x +1))
+
+merryChristmas(10)
